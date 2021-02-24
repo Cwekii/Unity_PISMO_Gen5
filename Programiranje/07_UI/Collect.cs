4316c0c615b3f105419eeba1ce4af1458257350c
Cwekii <35892622+Cwekii@users.noreply.github.com>
2021-02-24 12:49:16 +0100

    UI

---
+++ Programiranje/07_UI/Collect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collect : MonoBehaviour
+{
+    public Text scoreText;
+    int scoreCurrent;
+    int scoreMax;
+    GameObject[] objekti;
+
+    private void Start()
+    {
+        objekti = GameObject.FindGameObjectsWithTag("Coin");
+        scoreMax = objekti.Length;
+        scoreText.text = scoreCurrent + "/" + scoreMax;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coin")
+        {
+            scoreCurrent++;
+            Destroy(other.gameObject);
+            scoreText.text = scoreCurrent + "/" + scoreMax;
+        }
+    }
+}

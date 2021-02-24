a7e8c84276b2bd81f9b2c23d324922c373d95b25
Cwekii <35892622+Cwekii@users.noreply.github.com>
2021-02-24 09:04:33 +0100

    Array

---
+++ Programiranje/06_Array/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Dodjelitit prefabe enemya")]
+    public GameObject[] enemies;
+    [Header("Dodjeliti prazne game objekte sa scene koji su spawn pointovi")]
+    public Transform[] spawnPoints;
+    [Header("Početno vrijeme za spawn enemya")]
+    [Range(1, 5)]
+    public float timer;
+    float timerReset;
+
+    private void Start()
+    {
+        timerReset = timer;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            //Prvi način - duži
+            int randomEnemy = Random.Range(0, enemies.Length);
+            int randomSpawn = Random.Range(0, spawnPoints.Length);
+            Instantiate(enemies[randomEnemy], spawnPoints[randomSpawn].position, spawnPoints[randomSpawn].rotation);
+            timer = timerReset;
+
+            //Drugi način - kraći
+            Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+            timer = timerReset;
+        }
+    }
+}

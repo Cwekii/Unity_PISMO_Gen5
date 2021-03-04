b76cb411e61c6e5347de9e03404327d03ed5cc45
Cwekii <35892622+Cwekii@users.noreply.github.com>
2021-03-04 08:52:43 +0100

    UI sound

---
+++ Programiranje/07_UI/VolumeSettings.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    public AudioSource ass;
+    public Slider slid;
+    public Text btnSoundText;
+    public Button btn;
+
+    private void Start()
+    {
+        slid.value = ass.volume;
+        if (ass.isPlaying == true)
+        {
+            btnSoundText.text = "ON";
+            var btnColors = btn.colors;
+            btnColors.selectedColor = Color.green;
+        }
+        else
+        {
+            btnSoundText.text = "OFF";
+            var btnColors = btn.colors;
+            btnColors.selectedColor = Color.red;
+        }
+
+    }
+
+    public void ChangeAudio()
+    {
+        ass.volume = slid.value;
+    }
+
+    public void OnOffAudio()
+    {
+        var btnColors = btn.colors;
+        if (ass.isPlaying == true)
+        {
+            ass.Stop();
+            btnSoundText.text = "OFF";
+            btnColors.selectedColor = Color.red;
+
+        }
+        else
+        {
+            ass.Play();
+            btnSoundText.text = "ON";
+            btnColors.selectedColor = Color.green;
+
+        }
+    }
+}

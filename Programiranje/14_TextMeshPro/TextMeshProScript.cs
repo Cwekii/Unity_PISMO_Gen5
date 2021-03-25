77c8289925656bd809ba4c320459bc74973641f9
Cwekii <dominik.cvetkovski@gmail.com>
2021-03-25 10:43:47 +0100

    Text Mesh Pro

---
+++ Programiranje/14_TextMeshPro/TextMeshProScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextMeshProScript : MonoBehaviour
+{
+    public TMP_Text textic;
+    int broj = 0;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            broj++;
+            textic.text = broj.ToString();
+        }
+    }
+}

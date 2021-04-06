15af2d928c6f95bd4594e53e056c0a5fb5a40dbf
Cwekii <dominik.cvetkovski@gmail.com>
2021-04-06 10:10:23 +0200

    Optimizacija

---
+++ Programiranje/17_DontDestroy_AsyncLoad/Zadatak.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Zadatak : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Scena gore
+            PlayerPrefs.SetInt("scene", SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene(0);
+        }
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            //Scena dolje
+            PlayerPrefs.SetInt("scene", SceneManager.GetActiveScene().buildIndex - 1);
+            SceneManager.LoadScene(0);
+        }
+    }
+}

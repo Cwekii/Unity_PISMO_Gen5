93a894be796a447e7a4964c92fa3037f1c22480e
Cwekii <dominik.cvetkovski@gmail.com>
2021-04-26 11:42:47 +0200

    Translate

---
+++ Programiranje/22_Translate/ChangeLanguage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLanguage : MonoBehaviour
+{
+    public string jezik = "en";
+
+    public void KlikniDaPromjenisJezik()
+    {
+        PlayerPrefs.SetString("Language", jezik);
+        Debug.Log("Saveano u playerPrefs");
+
+        LanguageTranslation.LoadLangugae();
+        Debug.Log("Uspiješno loadano");
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //restiraj scenu da se prikaže jezik
+        Debug.Log("Resetirano");
+    }
+}
---
+++ Programiranje/22_Translate/LanguageText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageText : MonoBehaviour
+{
+    public string textKey;
+
+    private void Start()
+    {
+        Text textic = GetComponent<Text>();
+        if(textic != null)
+        {
+            if(textic.text == "ISOcode")
+            {
+                textic.text = LanguageTranslation.GetLanguage();
+            }
+            else
+            {
+                textic.text = LanguageTranslation.Fields[textKey];
+            }
+        }
+    }
+}

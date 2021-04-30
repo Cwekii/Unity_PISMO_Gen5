174af9900c805900d2e78012b19cbdbcb0fd88ab
Cwekii <dominik.cvetkovski@gmail.com>
2021-04-30 13:53:31 +0200

    Prezentacije i Share

---
+++ Programiranje/23_Share/Share.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Share : MonoBehaviour
+{
+    //Twitter
+    string Twitter_Adress = "https://twitter.com/intent/tweet";
+    string tweet_Language = "en";
+    string textToDisplay = "#SuckMyDick " + " Hey friends! Check out my score: "  + " in this dick sucking game";
+
+    //Facebook
+    string facebook_Adress = "https://www.facebook.com/dialog/feed?";
+    string AppID = "23762215393"; //Morate svoji app id - https://developers.facebook.com/apps/
+    string link = "https://www.youtube.com/watch?v=WOjl8c2Rp9o";
+    string picture = "";
+    string caption = "Check out my new score!";
+    string description = "Enjoy fun, free games at pornhub.com";
+
+    public void ShareOnTwitter()
+    {
+        Application.OpenURL(Twitter_Adress + "?text=" + UnityWebRequest.EscapeURL(textToDisplay) + "&amp;lang=" + tweet_Language);
+    }
+
+    public void ShareOnFacebook()
+    {
+        Application.OpenURL(facebook_Adress + "app_id=" + AppID + "&link=" + link + "&picture=" + picture
+            + "&caption" + caption + PlayerPrefs.GetInt("Highscore").ToString() + "&description=" + UnityWebRequest.EscapeURL(description));
+    }
+}
---
+++ Programiranje/23_Share/ShareUltimateButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+//Za koristiti ovu skriptu morate imati Native Share Plugin iz Asset Storea
+//https://assetstore.unity.com/packages/tools/integration/native-share-for-android-ios-112731
+public class ShareUltimateButton : MonoBehaviour
+{
+    [SerializeField] GameObject screenSHootPanel;
+
+    public void OnShare()
+    {
+        screenSHootPanel.SetActive(true);
+        StartCoroutine(TakeScreenshotAndShareIt());
+    }
+
+    IEnumerator TakeScreenshotAndShareIt()
+    {
+        yield return new WaitForSeconds(0.5f);
+        //ss skraceno za ScreenShoot
+        //Novo koristeno TextureFormat.RGBA32
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
+
+        string filePath = Path.Combine(Application.temporaryCachePath, "shared_img.png");
+        File.WriteAllBytes(filePath, ss.EncodeToPNG());
+
+        Destroy(ss);
+
+        new NativeShare().AddFile(filePath).SetSubject("Highscore Cool Game").
+            SetText("Check out my New HIGHSCORE: " + PlayerPrefs.GetInt("Highscore").ToString()).Share();
+        screenSHootPanel.SetActive(false);
+    }
+}

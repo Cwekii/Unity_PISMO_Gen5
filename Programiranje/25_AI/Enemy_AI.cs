d2bbba4cfde9dbfdb6d7d2fedce6c373acadbbb6
Cwekii <dominik.cvetkovski@gmail.com>
2021-05-17 12:20:20 +0200

    AI i Ispiti

---
+++ Programiranje/25_AI/Enemy_AI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy_AI : MonoBehaviour
+{
+    public Transform player; //Objekt do kojeg enemy AI treba ići (pratiti)
+    Vector3 finalDestionation; //Konačna pozicija na koju enemy treba doći
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = FindObjectOfType<CharacterController>().gameObject.transform;
+        finalDestionation = agent.destination;
+    }
+
+    private void LateUpdate()
+    {
+        if(Vector3.Distance(finalDestionation, player.position) > 2.0f)
+        {
+            finalDestionation = player.position;
+            agent.destination = finalDestionation;
+        }
+    }
+}

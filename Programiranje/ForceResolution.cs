422d5e2e56e4c9b50f1363bb79e8d4daab09355c
Cwekii <dominik.cvetkovski@gmail.com>
2021-07-07 11:44:49 +0200

    Resolution

---
+++ Programiranje/ForceResolution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceResolution : MonoBehaviour
+{
+    public GameObject canvas16_9;
+    public GameObject canvas16_10;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        //9:16
+        if(Screen.height / Screen.width > 1.7f && Screen.height / Screen.width < 1.8f)
+        {
+            Screen.SetResolution(1080, 1920, true);
+            canvas16_9.SetActive(true);
+        }
+        //10:16
+        if(Screen.height / Screen.width == 1.6f)
+        {
+            Screen.SetResolution(800, 1280, true);
+        }
+        //4:3
+        //18:9
+    }
+}
